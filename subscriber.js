4aa434ba08a779d7c9a44fb64743b86ac16c1c3f
Mizanur Rahman <mizan@arogga.net>
2024-04-17 12:32:45 +0600

    used online broker

--- subscriber.js
+++ subscriber.js
@@ -1,6 +1,6 @@
 // subscriber.js
 const mqtt = require("mqtt");
-const client = mqtt.connect("mqtt://localhost:1883");
+const client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
 
 client.on("connect", function () {
   client.subscribe("test/topic", function (err) {
@@ -14,9 +14,11 @@ client.on("connect", function () {
 
 client.on("message", function (topic, message) {
   // message is Buffer, parse it as JSON
-  const messageObject = JSON.parse(message.toString());
-  console.log(`${topic}: ${messageObject.time} - ${messageObject.message}`);
-  console.log(`Data Array: ${messageObject.arr.join(", ")}`);
+  const msg = message.toString();
+  //   const messageObject = JSON.parse(message.toString());
+  //   console.log(`${topic}: ${messageObject.time} - ${messageObject.message}`);
+  //   console.log(`Data Array: ${messageObject.arr.join(", ")}`);
+  console.log(topic, msg);
 });
 
 client.on("error", function (error) {
